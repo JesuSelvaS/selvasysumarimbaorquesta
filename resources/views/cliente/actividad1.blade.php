eef1952e7a9cb05ce8b183245dbf37acab22e50a
JesuSelvaS <jassselvas@gmail.com>
2019-02-05 21:47:16 -0600

    jesus
    - otras cosas

--- resources/views/cliente/actividad1.blade.php
+++ resources/views/cliente/actividad1.blade.php
@@ -6,6 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Práctica 1 | Selvas y su Marimba Orquesta</title>
+
+    <link rel="stylesheet" href="{{asset('css/normalize.css')}}">
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
 </head>
 
 <body>
@@ -17,6 +20,7 @@
             <div class="teamInfo">
                 <div class="teamName">
                     <p>Equipo: Selvas y su Marimba Orquesta</p>
+                    <p>Asignatura: Tópicos Avanzados de Tecnologías Web</p>
                 </div>
                 <div class="teamMember">
                     <p>Cruz Rodas Edrey Alfredo</p>
@@ -28,17 +32,26 @@
                             <div class="infoItem">
                                 <!-- Información básica -->
                                 <h3>Personal</h3>
-                                <p></p>
+                                <ul>
+                                    <li>Fecha de nacimiento: 30 de noviembre de 1997</li>
+                                    <li>Género: Hombre</li>
+                                    <li>Dirección: 19 sur y 4 oriente</li>
+                                    <li>Estado Civil: Soltero</li>
+                                </ul>
                             </div>
                             <div class="infoItem">
                                 <!-- Gustos -->
                                 <h3>Gustos</h3>
-                                <p></p>
+                                <p>Desarrollar apps multiplataforma con c sharp con el framework xamarin</p>
                             </div>
                             <div class="infoItem">
                                 <!-- Hobbies -->
                                 <h3>Hobbies</h3>
-                                <p></p>
+                                <ul>
+                                    <li>Entrenamiento fisico</li>
+                                    <li>Tocar Guitarra</li>
+                                    <li>Leer</li>
+                                </ul>
                             </div>
                         </article>
                     </div>
@@ -48,17 +61,19 @@
                             <div class="infoItem">
                                 <!-- Trayectoria -->
                                 <h3>Trayectoria</h3>
-                                <p></p>
+                                <p>Desde que naci he vivido en varios lugares de Chiapas, por ejemplo durante mi educacion preescolar y primaria
+fue en Tapachula, desde sexto de primaria hasta primero de secundaria en Cintalapa, secundaria y preparatoria
+en Acacoyagua y finalmente la educacion superior en el TECNM Campus Tuxtla Gutièrrez.</p>
                             </div>
                             <div class="infoItem">
                                 <!-- Área de desempeño -->
                                 <h3>Área de desempeño</h3>
-                                <p></p>
+                                <p>Ingenieria en sistemas computacionales. Semestre 8</p>
                             </div>
                             <div class="infoItem">
                                 <!-- Meta -->
                                 <h3>Meta</h3>
-                                <p></p>
+                                <p>Terminar este grado superior de estudios</p>
                             </div>
                         </article>
                     </div>
@@ -69,6 +84,7 @@
             <div class="teamInfo">
                 <div class="teamName">
                     <p>Equipo: Selvas y su Marimba Orquesta</p>
+                    <p>Asignatura: Tópicos Avanzados de Tecnologías Web</p>
                 </div>
                 <div class="teamMember">
                     <p>González Ruíz Daniel Alejandro</p>
@@ -80,17 +96,32 @@
                             <div class="infoItem">
                                 <!-- Información básica -->
                                 <h3>Personal</h3>
-                                <p></p>
+                                <ul>
+                                    <li>Fecha de nacimiento: 12 de Abril de 1997</li>
+                                    <li>Género: Masculino</li>
+                                    <li>Dirección: 15av Sur Oriente N. 225 Col. San Francisco</li>
+                                    <li>Estado Civil: Soltero</li>
+                                </ul>
                             </div>
                             <div class="infoItem">
                                 <!-- Gustos -->
                                 <h3>Gustos</h3>
-                                <p></p>
+                                <ul>
+                                    <li>Jugar Videojuegos</li>
+                                    <li>Jugar fútbol</li>
+                                    <li>Caminar</li>
+                                    <li>Escuchar música</li>
+                                    <li>Practicar en programación especialmente en el lenguaje C Sharp</li>
+                                    <li>Hacer ejercicio</li>
+                                </ul>
                             </div>
                             <div class="infoItem">
                                 <!-- Hobbies -->
                                 <h3>Hobbies</h3>
-                                <p></p>
+                                <ul>
+                                    <li>Jugar videojuegos</li>
+                                    <li>Hacer ejercicio</li>
+                                </ul>
                             </div>
                         </article>
                     </div>
@@ -100,17 +131,17 @@
                             <div class="infoItem">
                                 <!-- Trayectoria -->
                                 <h3>Trayectoria</h3>
-                                <p></p>
+                                <p>Mi primera formación educativa comenzó en la escuela Primaria Emiliano Zapata Salazar, posteriormente estudie mi secundaria en la Secundaria Técnica N.50, luego mi educación media superior en el COBACH Plantel 15 que estas tres escuelas pertenecen al municipio de La Concordia, Chiapas. Actualmente estoy estudiando la universidad en el Instituto Tecnológico de Tuxtla Gutiérrez.</p>
                             </div>
                             <div class="infoItem">
                                 <!-- Área de desempeño -->
                                 <h3>Área de desempeño</h3>
-                                <p></p>
+                                <p>Ingeniería En Sistemas Computacionales. Semestre 8</p>
                             </div>
                             <div class="infoItem">
                                 <!-- Meta -->
                                 <h3>Meta</h3>
-                                <p></p>
+                                <p>Terminar mi carrera e irme a Tijuana con mi Chaparritha ♥</p>
                             </div>
                         </article>
                     </div>
@@ -121,6 +152,7 @@
             <div class="teamInfo">
                 <div class="teamName">
                     <p>Equipo: Selvas y su Marimba Orquesta</p>
+                    <p>Asignatura: Tópicos Avanzados de Tecnologías Web</p>
                 </div>
                 <div class="teamMember">
                     <p>Gutiérrez Gutiérrez José Leyder</p>
@@ -132,17 +164,22 @@
                             <div class="infoItem">
                                 <!-- Información básica -->
                                 <h3>Personal</h3>
-                                <p></p>
+                                <ul>
+                                    <li>Fecha de nacimiento: 26 de Julio de 1997</li>
+                                    <li>Genero: Hombre</li>
+                                    <li>Dirección: Terán</li>
+                                    <li>Estado civil: Soltero</li>
+                                </ul>
                             </div>
                             <div class="infoItem">
                                 <!-- Gustos -->
                                 <h3>Gustos</h3>
-                                <p></p>
+                                <p>Me gusta trabajar con Bases de Datos asi como programar en C#, además de convivir con mis amigos</p>
                             </div>
                             <div class="infoItem">
                                 <!-- Hobbies -->
                                 <h3>Hobbies</h3>
-                                <p></p>
+                                <p>Jugar Futbol</p>
                             </div>
                         </article>
                     </div>
@@ -152,17 +189,20 @@
                             <div class="infoItem">
                                 <!-- Trayectoria -->
                                 <h3>Trayectoria</h3>
-                                <p></p>
+                                <p>Mi primaria la curse en la escuela Lic. Benito Juarez la cual se encontraba en el pueblo en que nací,
+                                    así como la secundaria en la que estudie. Mi preparatoria la curse en un municipio lejos del lugar en el que nací 
+                                    y actualmente me encuentro cursando mi licenciatura en el Instituto Tecnológico de Tuxtla Gutiérrez
+                                </p>
                             </div>
                             <div class="infoItem">
                                 <!-- Área de desempeño -->
                                 <h3>Área de desempeño</h3>
-                                <p></p>
+                                <p>Ing. Sistemas computacionales. Semestre 8</p>
                             </div>
                             <div class="infoItem">
                                 <!-- Meta -->
                                 <h3>Meta</h3>
-                                <p></p>
+                                <p>La meta más sercana que tengo es terminar mi carrera</p>
                             </div>
                         </article>
                     </div>
@@ -173,6 +213,7 @@
             <div class="teamInfo">
                 <div class="teamName">
                     <p>Equipo: Selvas y su Marimba Orquesta</p>
+                    <p>Asignatura: Tópicos Avanzados de Tecnologías Web</p>
                 </div>
                 <div class="teamMember">
                     <p>Sánchez Selvas Jesús Alberto</p>
@@ -184,17 +225,32 @@
                             <div class="infoItem">
                                 <!-- Información básica -->
                                 <h3>Personal</h3>
-                                <p></p>
+                                <ul>
+                                    <li>Fecha de nacimiento: 30 de Junio de 1997</li>
+                                    <li>Género: Masculino</li>
+                                    <li>Dirección: 4 Sur entre 10 y 11 Ote #1111</li>
+                                    <li>Estado Civil: Soltero :'c</li>
+                                </ul>
                             </div>
                             <div class="infoItem">
                                 <!-- Gustos -->
                                 <h3>Gustos</h3>
-                                <p></p>
+                                <ul>
+                                    <li>Practicar programación</li>
+                                    <li>Jugar videojuegos</li>
+                                    <li>Flan Napolitano</li>
+                                    <li>El color Azul</li>
+                                    <li>Estar produciendo</li>
+                                </ul>
                             </div>
                             <div class="infoItem">
                                 <!-- Hobbies -->
                                 <h3>Hobbies</h3>
-                                <p></p>
+                                <ul>
+                                    <li>Comparar tendencias de diseño actuales en Web</li>
+                                    <li>Jugar con mis amigos cualquier deporte</li>
+                                    <li>Enseñar los conocimientos que dispongo</li>
+                                </ul>
                             </div>
                         </article>
                     </div>
@@ -204,17 +260,17 @@
                             <div class="infoItem">
                                 <!-- Trayectoria -->
                                 <h3>Trayectoria</h3>
-                                <p></p>
+                                <p>Empecé a estudiar el preescolar en la instutición Leona Vicario atrás del mercado del 5 de mayo. Estudié en la primaria Juan Benavides frente al 5 de mayo. Estudié en la Escuela Secundaria del Estado. Posteriormente, estudié en el CBTis 144 y ahora me encuentro estudiando la carrera de ISC en el Instituto Tecnológico de Tuxtla Gutiérrez.</p>
                             </div>
                             <div class="infoItem">
                                 <!-- Área de desempeño -->
                                 <h3>Área de desempeño</h3>
-                                <p></p>
+                                <p>Ing. Sistemas Computaciónales. Semestre 8</p>
                             </div>
                             <div class="infoItem">
                                 <!-- Meta -->
                                 <h3>Meta</h3>
-                                <p></p>
+                                <p>Ser un Full Stack Developer para serle mucho más útil a mis compañeros de trabajo e impulsar a nuestra empresa.</p>
                             </div>
                         </article>
                     </div>
@@ -225,6 +281,7 @@
             <div class="teamInfo">
                 <div class="teamName">
                     <p>Equipo: Selvas y su Marimba Orquesta</p>
+                    <p>Asignatura: Tópicos Avanzados de Tecnologías Web</p>
                 </div>
                 <div class="teamMember">
                     <p>Sol Santiago Sheyla Valeria</p>
@@ -236,17 +293,23 @@
                             <div class="infoItem">
                                 <!-- Información básica -->
                                 <h3>Personal</h3>
-                                <p></p>
+                                <ul>
+                                    <li>Fecha de nacimiento: 20 de junio de 1997</li>
+                                    <li>Edad: 21 años</li>
+                                    <li>Género: Mujer</li>
+                                    <li>Dirección: 15 av Sur poniente, Tuxtla Gutiérrez Chiapas</li>
+                                    <li>Estado civil: Soltera</li>
+                                </ul>
                             </div>
                             <div class="infoItem">
                                 <!-- Gustos -->
                                 <h3>Gustos</h3>
-                                <p></p>
+                                <p>Me gusta el lenguaje html, el color Azul y los Girasoles ♥</p>
                             </div>
                             <div class="infoItem">
                                 <!-- Hobbies -->
                                 <h3>Hobbies</h3>
-                                <p></p>
+                                <p>Me encantan las series de fantasía y ciencia ficción</p>
                             </div>
                         </article>
                     </div>
@@ -256,17 +319,22 @@
                             <div class="infoItem">
                                 <!-- Trayectoria -->
                                 <h3>Trayectoria</h3>
-                                <p></p>
+                                <ul>
+                                    <li>Primaria: 21 de agosto</li>
+                                    <li>Secundaria: Ponciano Arriaga, San Luis Potosí, Y Moisés Saenz Garza Tuxtla Gutiérrez</li>
+                                    <li>Preparatoria: Cobach 145</li>
+                                    <li>Universidad: instituto tecnológico de Tuxtla Gutiérrez</li>
+                                </ul>
                             </div>
                             <div class="infoItem">
                                 <!-- Área de desempeño -->
                                 <h3>Área de desempeño</h3>
-                                <p></p>
+                                <p>Ing. Sistemas Computaciónales. Semestre 8</p>
                             </div>
                             <div class="infoItem">
                                 <!-- Meta -->
                                 <h3>Meta</h3>
-                                <p></p>
+                                <p>Estudiar mi maestría en San Luis Potosí en ciencias de computación</p>
                             </div>
                         </article>
                     </div>
@@ -290,6 +358,10 @@
     </div>
 
     <footer></footer>
+
+    <script type="text/javascript" src="{{asset('js/jquery.js')}}"></script>
+    <script type="text/javascript" src="{{asset('js/bootstrap.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('js/popper.min.js')}}"></script>
 </body>
 
 </html>
