9c332f8c5936a312e63e44a3c11e0189ccf6b984
JesuSelvaS <jassselvas@gmail.com>
2019-03-02 22:06:31 -0600

    yisus
    - se añadieron clases de bootstrap

--- resources/views/cliente/actividad3.blade.php
+++ resources/views/cliente/actividad3.blade.php
@@ -9,24 +9,24 @@
         <!-- Insertar la galería de Bootstrap 4 con las descripciones, indicadores, y botones, solo copia y pega desde la página oficial :3 -->
     </section>
     <section class="miniAbout">
-        <div class="informacionServiciosContent">
-            <div class="aboutDescription">
-                <div class="imageDescription">
+        <div class="informacionServiciosContent container">
+            <div class="aboutDescription row">
+                <div class="imageDescription col-md-4">
                     <figure>
                         <img src="" alt="imageAbout">
                     </figure>
                 </div>
-                <div class="info">
+                <div class="info col-md-8">
                     <h2>¿Qué ofrece nuestro modelo educativo?</h2>
                     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate inventore consequatur reiciendis itaque autem, optio repellendus omnis eius fuga accusamus voluptate, error blanditiis ex, unde voluptates tenetur qui dolorum? Ipsam!</p>
                     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim sequi at sunt quas distinctio ipsum quos. Nobis dolorum ex culpa officia, ipsam quos iusto harum cum voluptatibus numquam dolor tenetur.</p>
                 </div>
             </div>
-            <div class="servicesContent">
-                <div class="titleServices">
+            <div class="servicesContent row">
+                <div class="titleServices col-md-12">
                     <h2>¿Porqué Elegirnos?</h2>
                 </div>
-                <div class="serviceItem">
+                <div class="serviceItem col-md-4">
                     <div class="iconService">
                         <!-- Aquí va el ícono -->
                     </div>
@@ -35,7 +35,7 @@
                         <p>Aquí información breve del servicio</p>
                     </div>
                 </div>
-                <div class="serviceItem">
+                <div class="serviceItem col-md-4">
                     <div class="iconService">
                         <!-- Aquí va el ícono -->
                     </div>
@@ -44,7 +44,7 @@
                         <p>Aquí información breve del servicio</p>
                     </div>
                 </div>
-                <div class="serviceItem">
+                <div class="serviceItem col-md-4">
                     <div class="iconService">
                         <!-- Aquí va el ícono -->
                     </div>
@@ -57,84 +57,114 @@
         </div>
     </section>
     <section class="bestModulos">
-        <div class="modulosContent">
-            <div class="modulo">
-                <div class="moduloImage">
-                    <figure>
-                        <img src="" alt="moduloImage">
-                    </figure>
+        <div class="modulosContent container">
+            <div class="row">
+                <div class="modulo col-md-4">
+                    <div class="moduloImage">
+                        <figure>
+                            <img src="" alt="moduloImage">
+                        </figure>
+                    </div>
+                    <div class="moduloTitle">
+                        <h3>Ingeniería en Sistemas Computacionales</h3>
+                    </div>
+                    <div class="descModulo">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, officia! Nihil quod, quia nesciunt sed cupiditate pariatur iste vel repellendus debitis libero facere assumenda, consequuntur, error mollitia et natus eum.</p>
+                    </div>
                 </div>
-                <div class="moduloTitle">
-                    <h3>Ingeniería en Sistemas Computacionales</h3>
+                <div class="modulo col-md-4">
+                    <div class="moduloImage">
+                        <figure>
+                            <img src="" alt="moduloImage">
+                        </figure>
+                    </div>
+                    <div class="moduloTitle">
+                        <h3>Ingeniería en Gestión Empresarial</h3>
+                    </div>
+                    <div class="descModulo">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, officia! Nihil quod, quia nesciunt sed cupiditate pariatur iste vel repellendus debitis libero facere assumenda, consequuntur, error mollitia et natus eum.</p>
+                    </div>
                 </div>
-                <div class="descModulo">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, officia! Nihil quod, quia nesciunt sed cupiditate pariatur iste vel repellendus debitis libero facere assumenda, consequuntur, error mollitia et natus eum.</p>
+                <div class="modulo col-md-4">
+                    <div class="moduloImage">
+                        <figure>
+                            <img src="" alt="moduloImage">
+                        </figure>
+                    </div>
+                    <div class="moduloTitle">
+                        <h3>Ingeniería en Bioquímica</h3>
+                    </div>
+                    <div class="descModulo">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, officia! Nihil quod, quia nesciunt sed cupiditate pariatur iste vel repellendus debitis libero facere assumenda, consequuntur, error mollitia et natus eum.</p>
+                    </div>
                 </div>
-            </div>
-            <div class="modulo">
-                <div class="moduloImage">
-                    <figure>
-                        <img src="" alt="moduloImage">
-                    </figure>
+                <div class="modulo col-md-4">
+                    <div class="moduloImage">
+                        <figure>
+                            <img src="" alt="moduloImage">
+                        </figure>
+                    </div>
+                    <div class="moduloTitle">
+                        <h3>Ingeniería en Bioquímica</h3>
+                    </div>
+                    <div class="descModulo">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, officia! Nihil quod, quia nesciunt sed cupiditate pariatur iste vel repellendus debitis libero facere assumenda, consequuntur, error mollitia et natus eum.</p>
+                    </div>
                 </div>
-                <div class="moduloTitle">
-                    <h3>Ingeniería en Gestión Empresarial</h3>
-                </div>
-                <div class="descModulo">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, officia! Nihil quod, quia nesciunt sed cupiditate pariatur iste vel repellendus debitis libero facere assumenda, consequuntur, error mollitia et natus eum.</p>
-                </div>
-            </div>
-            <div class="modulo">
-                <div class="moduloImage">
-                    <figure>
-                        <img src="" alt="moduloImage">
-                    </figure>
-                </div>
-                <div class="moduloTitle">
-                    <h3>Ingeniería en Bioquímica</h3>
-                </div>
-                <div class="descModulo">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, officia! Nihil quod, quia nesciunt sed cupiditate pariatur iste vel repellendus debitis libero facere assumenda, consequuntur, error mollitia et natus eum.</p>
+                <div class="modulo col-md-4">
+                    <div class="moduloImage">
+                        <figure>
+                            <img src="" alt="moduloImage">
+                        </figure>
+                    </div>
+                    <div class="moduloTitle">
+                        <h3>Ingeniería en Bioquímica</h3>
+                    </div>
+                    <div class="descModulo">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, officia! Nihil quod, quia nesciunt sed cupiditate pariatur iste vel repellendus debitis libero facere assumenda, consequuntur, error mollitia et natus eum.</p>
+                    </div>
                 </div>
             </div>
         </div>
     </section>
 </main>
-<footer>
-    <div class="footerCol">
-        <div class="title">
-            <h3>Integrantes</h3>
+<footer class="container">
+    <div class="row">
+        <div class="footerCol col-md-4">
+            <div class="title">
+                <h3>Integrantes</h3>
+            </div>
+            <div class="description">
+                <ul>
+                    <li>Cruz Rodas Edrey Alfredo - 15270733</li>
+                    <li>González Ruíz Daniel Alejandro - 15270738</li>
+                    <li>Gutiérrez Gutiérrez José Leyder - 15270740</li>
+                    <li>Sánchez Selvas Jesús Alberto - 15270794</li>
+                    <li>Sol Santiago Sheyla Valeria - 15270796</li>
+                </ul>
+            </div>
         </div>
-        <div class="description">
-            <ul>
-                <li>Cruz Rodas Edrey Alfredo - 15270733</li>
-                <li>González Ruíz Daniel Alejandro - 15270738</li>
-                <li>Gutiérrez Gutiérrez José Leyder - 15270740</li>
-                <li>Sánchez Selvas Jesús Alberto - 15270794</li>
-                <li>Sol Santiago Sheyla Valeria - 15270796</li>
-            </ul>
+        <div class="footerCol col-md-4">
+            <div class="title">
+                <h3>Links de interés</h3>
+            </div>
+            <div class="description">
+                <ul>
+                    <li><a href="#">Ing. En Gestión</a></li>
+                    <li><a href="#">Ing. En Mecánica</a></li>
+                    <li><a href="#">Ing. Industrial</a></li>
+                    <li><a href="#">Ing. Bioquímica</a></li>
+                </ul>
+            </div>
         </div>
-    </div>
-    <div class="footerCol">
-        <div class="title">
-            <h3>Links de interés</h3>
-        </div>
-        <div class="description">
-            <ul>
-                <li><a href="#">Ing. En Gestión</a></li>
-                <li><a href="#">Ing. En Mecánica</a></li>
-                <li><a href="#">Ing. Industrial</a></li>
-                <li><a href="#">Ing. Bioquímica</a></li>
-            </ul>
-        </div>
-    </div>
-    <div class="footerCol">
-        <div class="title">
-            <h3>Contacto</h3>
-        </div>
-        <div class="description">
-            <p>Vaya a la sección de contactos por cualquier duda.</p>
-            <a href="#"><button>IR A CONTACTOS</button></a>
+        <div class="footerCol col-md-4">
+            <div class="title">
+                <h3>Contacto</h3>
+            </div>
+            <div class="description">
+                <p>Vaya a la sección de contactos por cualquier duda.</p>
+                <a href="#"><button>IR A CONTACTOS</button></a>
+            </div>
         </div>
     </div>
 </footer>
